d1f444197477f93f74a46cece5ea9576564ae478
InsertTextHereInsertTextHere <damien.leongpm@gmail.com>
2024-06-20 22:41:18 +0800

    Last updated: alpha 1.0

--- Sorting.py
+++ Sorting.py
@@ -1,3 +1,17 @@
+# This is the sorting algorithmn visualiser
+# Version alpha 1.0
+
 import pygame
 
 pygame.init()
+screenWidth = 960
+screenHeight = 540
+
+mainWindow = pygame.display.set_mode((screenWidth, screenHeight))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    pygame.display.update()
